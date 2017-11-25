23238ebef38f63b593c5580357ef5700ff493857
jack0lantern <j_j_jiang@yahoo.com>
2017-11-24 18:29:42 -0600

    cursor img added to center of screen

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour
+{
+	public float cursorSize = 50;
+
+	Rect crosshairRect;
+	Texture crosshairTexture;
+
+	void Start()
+	{
+		float crosshairSize = Screen.width * 0.1f * cursorSize / 100;
+		crosshairTexture = Resources.Load ("Textures/mousepointer") as Texture;
+		crosshairRect = new Rect (Screen.width / 2 - crosshairSize / 2,
+			Screen.height / 2 - crosshairSize / 2,
+			crosshairSize, crosshairSize);
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture (crosshairRect, crosshairTexture);
+	}
+}
+
