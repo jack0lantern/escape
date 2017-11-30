0209d038a116e4acf8af783d44090b4cc64419e2
jack0lantern <j_j_jiang@yahoo.com>
2017-11-29 22:04:49 -0600

    backup before upgrading scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,34 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 7.0f;
     public Text winText;
+    public float reach = 1000f;
+    Camera cam;
 
     void Start()
     {
+        cam = GetComponent<Camera>();
 		if (winText)
         	winText.text = "";
     }
 
+    void PerformInteraction()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, reach))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            PerformInteraction();
+        }
+    }
+
+    // Moves as fast as frame rate to smooth moving
     void FixedUpdate()
     {
 		float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
