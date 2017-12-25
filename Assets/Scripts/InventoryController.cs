66dc2eedd7de54dc5df21bae35b63304ff603873
jack0lantern <jack0lantern@users.noreply.github.com>
2017-12-24 23:55:35 -0600

    added files for inv panel functionality

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InventoryController : MonoBehaviour
+{
+	public Person player;
+
+	void Update()
+	{
+		Transform inventorySlots = gameObject.transform.GetChild (0);
+		for (int i = 0; i < player.inventory.Length; ++i) {
+			Button butt = inventorySlots.GetChild (i).GetComponent<Button> ();
+			if (player.inventory [i]) {
+				butt.image.sprite = player.inventory [i].invSprite;
+			}
+		}
+	}
+}
+
