0edd6fbb6330dac53c78f3682e16eb84f446949c
jack0lantern <j_j_jiang@yahoo.com>
2018-01-29 21:27:07 -0600

    woops forgot to add all these important files

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : Interactable {
+
+    private Animator _animator;
+	// Use this for initialization
+	void Start () {
+        _animator = GetComponent<Animator>();
+	}
+
+    public override void Interact(Person who)
+    {
+        Debug.Log("Door Interact!!");
+        Collectible selected = who.SelectedItem();
+        if (!_animator.GetBool("Open") && selected != null && selected.name == "Main Key")
+        {
+            _animator.SetBool("Open", true);
+            who.Drop();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Obsolete... or is it? (thinking of putting a showPanels in playercontroller instead)
+public class Inventory : MonoBehaviour
+{
+    public Person player;
+    private bool inventoryToggled;
+
+    void DisplayInventory()
+    {
+        Debug.Log("Display inventory");
+        // player.inventory
+    }
+
+    void HideInventory()
+    {
+        Debug.Log("Hide inventory");
+        // 
+    }
+
+    void ToggleInventory()
+    {
+        inventoryToggled = !inventoryToggled;
+        if (inventoryToggled)
+        {
+            DisplayInventory();
+        }
+        else
+        {
+            HideInventory();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInventory();
+        }
+    }
+}
